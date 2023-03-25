3714b1583885c4a1f2c0003091e8acab8057d79a
javid rasouli <68181550+javidrasouli@users.noreply.github.com>
2023-03-25 20:53:43 +0430

    add response ...

--- errors/errors.go
+++ errors/errors.go
@@ -1,17 +1,55 @@
 package errors
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 type ResponseErrors struct {
 	ErrorType struct {
 		Title string `json:"title"`
 		Desc  string `json:"desc"`
 		Url   string `json:"url"`
 	} `json:"error_type"`
-	StatusKey string `json:"status_key"`
-	Detail    string `json:"detail"`
-	Title     string `json:"title"`
-	HelpUrl   string `json:"help_url"`
+	StatusKey string            `json:"status_key"`
+	Detail    string            `json:"detail"`
+	Title     string            `json:"title"`
+	Meta      map[string]string `json:"meta"`
+	HelpUrl   string            `json:"help_url"`
 }
 
-func New(key, entity, params map[string]string) *ResponseErrors {
+func getError(key string, account string, lang string, params map[string]string) *ResponseErrors {
+	req, err := http.NewRequest("GET", os.Getenv("coreApi")+"/api/core/errors/key/"+key, nil)
+	if err != nil {
+		return nil
+	}
+	req.Header.Set("account_uuid", account)
+	q := req.URL.Query()
+	q.Add("lang", lang)
+	paramsS, _ := json.Marshal(params)
+	q.Add("params", string(paramsS))
+	req.URL.RawQuery = q.Encode()
+	client := &http.Client{
+		Transport: &http.Transport{},
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil
+	}
+	Info := &ResponseErrors{}
+	err = json.Unmarshal(body, Info)
+	if err != nil {
+		return nil
+	}
+	return Info
+}
+
+func New(key, entity string, developerInfo string, params map[string]string) *ResponseErrors {
 
 }
--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,1 +1,13 @@
 package jwt
+
+func Validate(token string) bool {
+	return true
+}
+
+func FindValue(key string) interface{} {
+	return nil
+}
+
+func Open() map[string]any {
+	return nil
+}
