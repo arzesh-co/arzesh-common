028d0222c0bca94f6928ceb92867bc92ceffebfe
javid <javid99.rasouli@gmail.com>
2023-04-04 09:18:57 +0330

    fixed cycle imports

--- date/date.go
+++ date/date.go
@@ -1,9 +1,31 @@
 package date
 
 import (
-	"github.com/arzesh-co/arzesh-common/tools"
+	"strconv"
 	"time"
 )
+
+func convertorToInt64(n any) int64 {
+	switch n := n.(type) {
+	case int:
+		return int64(n)
+	case int8:
+		return int64(n)
+	case int16:
+		return int64(n)
+	case int32:
+		return int64(n)
+	case int64:
+		return int64(n)
+	case string:
+		num, err := strconv.ParseInt(n, 10, 64)
+		if err == nil {
+			return int64(0)
+		}
+		return num
+	}
+	return int64(0)
+}
 
 func UnixTimeStampToDate(timeStamp int64) time.Time {
 	return time.Unix(timeStamp, 0)
@@ -31,11 +53,11 @@ func StartToday(days any) any {
 	return theTime.Unix()
 }
 func NDayBefore(days any) any {
-	intDay := tools.ConvertorToInt64(days)
+	intDay := convertorToInt64(days)
 	return time.Now().Add((time.Duration(intDay) * -24) * time.Hour).Unix()
 }
 func NDayAfter(days any) any {
-	intDay := tools.ConvertorToInt64(days)
+	intDay := convertorToInt64(days)
 	return time.Now().Add((time.Duration(intDay) * -24) * time.Hour).Unix()
 }
 func BeginOfThisYear(days any) any {
